b5ee0e593a99c72aa735a101d45e51bf3b0660d7
Jeremy Castagno <jeremybyu@gmail.com>
2022-11-17 09:14:34 -0500

    Added custom classes and tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./templates/**/*.html", "./theme/**/*.html",  "./themes/"],
   theme: {
-    extend: {}
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '769px',
+    //   xl: '1440px',
+    // },
+
+
+    extend: {
+      // colors: {
+      //   'teal': {
+      //       100: "#d2f0ff",
+      //       200: "#a5e2ff",
+      //       300: "#79d3ff",
+      //       400: "#4cc5ff",
+      //       500: "#1fb6ff",
+      //       600: "#1992cc",
+      //       700: "#136d99",
+      //       800: "#0c4966",
+      //       900: "#062433"
+      //     }
+      // },
+    }
   },
   variants: {},
   plugins: [
